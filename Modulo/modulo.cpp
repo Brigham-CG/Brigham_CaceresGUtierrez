3b57b7d218bb48569e677398dec876f1d21cfe0a
Brigham <tr4t1nk@localhost.localdomain>
2021-04-15 13:24:19 -0500

    Complete

---
+++ Modulo/modulo.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int modulo(int a, int n){
+
+    int base = a - (a/n) * n;
+    if (a > 0){
+        if ( n > 0)
+            return base;
+        else
+            return base + n;
+        
+    }
+    else
+        if (n > 0)
+            return base + n;
+        else
+            return base;
+}
+
+int main(){
+    int a, n;
+    cout << "ingrese a: "; cin >> a;
+    cout << "ingrese n: "; cin >> n;
+    cout << "a mod n = " << modulo(a, n);
+}
